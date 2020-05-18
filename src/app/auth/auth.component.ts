d53b03c267fcdea581493b17b562e7f9445fca85
aligamal <aligamal@3ddx.com>
2020-05-18 02:39:36 +0200

    add validation to login form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -11,26 +11,38 @@ import { AuthenticationService } from '../services/authentication.service'
 export class AuthComponent implements OnInit {
   
   auth_flag = false ;
-  registerForm: FormGroup;
-  loginForm: FormGroup;
+  emailRegEx = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[.]+[com | net]{3}$';
+
+  registerForm = new FormGroup({
+    fullName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    address: new FormControl('')
+      });
+    
+    loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.pattern(this.emailRegEx)
+            ]),
+    password: new FormControl('',[Validators.required]),
+     });
+
+     
+
+  get email() { return this.loginForm.get('email'); }
+
+  get password() { return this.loginForm.get('password'); }
+
 
   constructor(private authService : AuthenticationService) { 
 
   }
 
   ngOnInit() {
-    this.registerForm = new FormGroup({
-      fullName: new FormControl(''),
-      email: new FormControl(''),
-      password: new FormControl(''),
-      phoneNumber: new FormControl(''),
-      address: new FormControl('')
-        });
-      
-      this.loginForm = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(''),
-       });
+    
   }
 
   onSubmit (form:FormGroup,flag:boolean)
@@ -42,7 +54,7 @@ export class AuthComponent implements OnInit {
         (data)=>{
             console.log("data="+JSON.stringify(data));
         },(err)=>{
-          console.log("error="+ JSON.stringify(err));
+          console.log("error="+ JSON.stringify(err.error.message));
         })
      }
      else
